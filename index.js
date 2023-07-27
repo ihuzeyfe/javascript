240100d8f4ec8607f0c0f76570073d65a93c3191
ihuzeyfe <ihuzeyfec@gmail.com>
2023-07-27 15:12:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+//document.write("merhaba!");
+// normal bir komut
+
+
+//var ad;
+//ad = prompt("Adınızı yazınız");
+//alert("Adınız " + ad)
+// Ad yazdırma
+
+//var sayi, kare;
+//sayi = prompt("Sayı Giriniz")
+//kare = sayi * sayi;
+//alert(sayi + " Sayısının karesi " + kare + " dir")
+// Girilen Sayının Karesi
+
+
+//var sayi1, sayi2, sayi3, toplam;
+//sayi1 = Number (prompt("1. Sayıyı giriniz")) ;
+//sayi2 = Number (prompt("2. Sayıyı giriniz"));
+//sayi3 = Number (prompt("3. Sayıyı giriniz"));
+//toplam = ((sayi1 + sayi2 + sayi3)/2);
+//alert(sayi1 + " , " + sayi2 + " ve " + sayi3 + "ün ortalamaları " + toplam + "dır")
+// 3 Sayının ortalaması
+
+//var sayi1, sayi2, cevre, alan;
+//sayi1 = Number (prompt("Dikdörtgenin kısa kenarının değerini giriniz"));
+//sayi2 = Number (prompt("Dikdörtgenin uzun kenarının değerini giriniz"));
+//cevre = ((sayi1 * 2) + (sayi2 * 2));
+//alan = sayi1 * sayi2;
+//alert("Dikdörtgenin çevresi" + cevre + " alanı ise " + alan);
+
+//var r, pi, cevre, alan;
+//pi = 3.14;
+//r = Number(prompt("lütfen yarıçapı değerini giriniz."))
+//cevre = 2 * pi * r;
+//alan = pi * r * r;
+//alert(" Dairenin alanı " + alan + " Dairenin çevresi" + cevre);
+//Dairenain alanı ve çevresi
+
+//var sayi, inch, donusum;
+//sayi = Number(prompt("Lütfen Sayı Giriniz."));
+//inc = 2.54;
+//don = 2.54 * sayi;
+//alert(sayi + " cm " + don + " inch etmektedir");
+//cm yi inch e çevirme;
+
