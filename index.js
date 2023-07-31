a429159e05a0e47362bcd81dc288b7b69286868b
ihuzeyfe <ihuzeyfec@gmail.com>
2023-07-31 10:28:44 +0300

    31 07 2023

--- index.js
+++ index.js
@@ -1,58 +1,15 @@
-//document.write("merhaba!");
-// normal bir komut
+
+function loadJS(file) {
+    var script = document.createElement("script");
+    script.src = file;
+    document.getElementsByTagName("head").item(0).appendChild(script);
+}
+
+// java dosyalarını yükle
+loadJS("./write.js");
+loadJS("./console.js");
+setTimeout(function() {
+    loadJS("./alert.js");
+}, 1000);
 
 
-//var ad;
-//ad = prompt("Adınızı yazınız");
-//alert("Adınız " + ad)
-// Ad yazdırma
-
-//var sayi, kare;
-//sayi = prompt("Sayı Giriniz")
-//kare = sayi * sayi;
-//alert(sayi + " Sayısının karesi " + kare + " dir")
-// Girilen Sayının Karesi
-
-
-//var sayi1, sayi2, sayi3, toplam;
-//sayi1 = Number (prompt("1. Sayıyı giriniz")) ;
-//sayi2 = Number (prompt("2. Sayıyı giriniz"));
-//sayi3 = Number (prompt("3. Sayıyı giriniz"));
-//toplam = ((sayi1 + sayi2 + sayi3)/2);
-//alert(sayi1 + " , " + sayi2 + " ve " + sayi3 + "ün ortalamaları " + toplam + "dır")
-// 3 Sayının ortalaması
-
-//var sayi1, sayi2, cevre, alan;
-//sayi1 = Number (prompt("Dikdörtgenin kısa kenarının değerini giriniz"));
-//sayi2 = Number (prompt("Dikdörtgenin uzun kenarının değerini giriniz"));
-//cevre = ((sayi1 * 2) + (sayi2 * 2));
-//alan = sayi1 * sayi2;
-//alert("Dikdörtgenin çevresi" + cevre + " alanı ise " + alan);
-
-//var r, pi, cevre, alan;
-//pi = 3.14;
-//r = Number(prompt("lütfen yarıçapı değerini giriniz."))
-//cevre = 2 * pi * r;
-//alan = pi * r * r;
-//alert(" Dairenin alanı " + alan + " Dairenin çevresi" + cevre);
-//Dairenain alanı ve çevresi
-
-//var sayi, inch, donusum;
-//sayi = Number(prompt("Lütfen Sayı Giriniz."));
-//inc = 2.54;
-//don = 2.54 * sayi;
-//alert(sayi + " cm " + don + " inch etmektedir");
-//cm yi inch e çevirme;
-
-//var c, f, don;
-//c = Number(prompt("Lüften Çevirmek İstediğiniz Santigrad dereceyi yazınız."))
-//f = (c * (1.8)) + 32;
-//alert(c + " santigrad derece " + f + " fahrenhayt derece yapar.");
-// santigrad dereceyi fahrenayta çeviren formul;
-
-//var a, b, x;
-//a = Number(prompt("Lütfen a değerini girinizi"));
-//b = Number(prompt("Lütfen b değerini girinizi"));
-//x = -(b / a);
-//alert("X değeri" + x + " dir");
-// Tek bilinmeyenli Denklem
